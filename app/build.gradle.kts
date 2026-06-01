bb3677b8d12614b3a0a9ad34b521eafcd4c6a038
Krystian Płoski <ploskik75@gmail.com>
2026-06-02 00:08:14 +0200

    1. Dodanie komentarzy w AndroidManifest 2. Przeniesienie klucza Google Maps z AndroidManifest do local.properties oraz konfiguracja manifestPlaceholders w build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,21 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
 }
 
+// Wczytanie pliku local.properties
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use {
+        localProperties.load(it)
+    }
+}
+
 android {
     namespace = "com.example.lokalizator_defibrylatorow"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.lokalizator_defibrylatorow"
@@ -18,13 +25,16 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Użycie wczytanego klucza
+        manifestPlaceholders["MAPS_API_KEY"] = localProperties.getProperty("MAPS_API_KEY")
+            ?: error("MAPS_API_KEY nie jest zdefiniowany w local.properties")
     }
 
-    // Moja sprawka
+    // Poprawiony blok buildFeatures
     buildFeatures {
         viewBinding = true
     }
-
 
     buildTypes {
         release {
@@ -35,6 +45,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -50,17 +61,17 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-// Retrofit + OkHttp
+    // Retrofit + OkHttp
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-// Kotlin Coroutines
+    // Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
-// AndroidX Lifecycle (ViewModel + LiveData + Runtime)
+    // AndroidX Lifecycle (ViewModel + LiveData + Runtime)
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
